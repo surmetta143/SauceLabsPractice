3e0bfbce16eb303858e17f4ee260461256d7adc9
surwsh <mskmouni@gmail.com>
2025-12-14 22:00:30 +0530

    ss

---
+++ TestCases/test_login.py
@@ -0,0 +1,19 @@
+import time
+
+from select import select
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+
+def test_login():
+    driver = webdriver.Chrome()
+    driver.get("https://testautomationpractice.blogspot.com/")
+    time.sleep(5)
+    driver.maximize_window()
+    driver.find_element(By.XPATH, "//button[@name='start']").click()
+    time.sleep(5)
+    driver.find_element(By.XPATH, "//button[@name='stop']").is_displayed()
+    driver.close()
+
+
