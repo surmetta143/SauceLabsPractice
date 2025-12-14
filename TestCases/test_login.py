aa6eeab6e2c93e4cfa1f24225e1ec84f2261dd13
surwsh <mskmouni@gmail.com>
2025-12-14 22:54:06 +0530

    dd

--- TestCases/test_login.py
+++ TestCases/test_login.py
@@ -3,11 +3,17 @@ import time
 from select import select
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
 
 
 
 def test_login():
-    driver = webdriver.Chrome()
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    driver = webdriver.Chrome(options=options)
     driver.get("https://testautomationpractice.blogspot.com/")
     time.sleep(5)
     driver.maximize_window()
