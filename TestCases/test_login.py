cb1e220970236cde4934e81c40a24e597827a36c
surwsh <mskmouni@gmail.com>
2025-12-15 21:27:20 +0530

    i am done this test case

--- TestCases/test_login.py
+++ TestCases/test_login.py
@@ -19,3 +19,14 @@ def test_login(open_chrome):
     driver.close()
 
 
+def test_login_rohit(open_chrome):
+    driver = open_chrome
+    driver.get("https://testautomationpractice.blogspot.com/")
+    time.sleep(5)
+    driver.maximize_window()
+    driver.find_element(By.XPATH, "//button[@name='start']").click()
+    time.sleep(5)
+    driver.find_element(By.XPATH, "//button[@name='stop']").is_displayed()
+    driver.close()
+
+
