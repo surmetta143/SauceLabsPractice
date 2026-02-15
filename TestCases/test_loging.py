08811c22d9a2a4b58689c4e658a942e7e9f24c7e
unknown <mskmouni@gmail.com>
2026-02-15 17:26:17 +0530

    Signed-off-by: unknown <mskmouni@gmail.com>

---
+++ TestCases/test_loging.py
@@ -0,0 +1,20 @@
+import time
+
+from select import select
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import pytest
+
+
+def test_stop_button(open_chrome):
+    driver = open_chrome
+
+    driver.get("https://testautomationpractice.blogspot.com/")
+    time.sleep(5)
+    driver.maximize_window()
+    driver.find_element(By.XPATH,"//button[@name='start']").click()
+    time.sleep(5)
+    driver.find_element(By.XPATH,"//button[@name='stop']").is_displayed()
+    driver.close()
+
